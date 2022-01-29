b6c4926e5f98d7c06f49782734275efb2ec17d78
Dan Vo <danvo.vo@gmail.com>
2022-01-29 14:45:48 -0800

    Satisfaction Survey v1

---
+++ app.py
@@ -0,0 +1,72 @@
+from flask import Flask, request, render_template, redirect, flash
+from flask_debugtoolbar import DebugToolbarExtension
+from surveys import satisfaction_survey as survey
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'secret'
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
+debug = DebugToolbarExtension(app)
+
+responses = []
+
+
+@app.route('/')
+def show_start():
+    """Generates start survey page"""
+    title = survey.title
+    instructions = survey.instructions
+
+    return render_template('start.html', title=title, instructions=instructions)
+
+
+@app.route('/start', methods=['POST'])
+def start_survey():
+    """Starts survey and redirects to first question"""
+    return redirect('/questions/0')
+
+
+@app.route('/questions/<int:id>')
+def show_question(id):
+    """Renders questions based on their ID"""
+
+    # redirects to home page if response list is empty, meaning survey has not started
+    if (len(responses) == []):
+        return redirect('/')
+
+    # if the length of the response list is equal to the length of the questions list, survey is complete.
+    if (len(responses) == len(survey.questions)):
+        return redirect('/done')
+
+    # if the user enters an invalid question ID into the URL, it will flash a message and redirect to the correct route
+    if (len(responses) != id):
+        flash(
+            f"Invalid question id: {id}. Please enter a valid question number.")
+
+        return redirect(f"questions/{len(responses)}")
+
+    # if none of those conditionals are met, survey continues in progress. Shows question on page.
+    question = survey.questions[id]
+
+    return render_template('questions.html', question=question, question_num=id)
+
+
+@app.route('/answer', methods=['POST'])
+def handle_questions():
+    """Collects user responses to questions"""
+
+    choice = request.form['answer']
+    responses.append(choice)
+
+    """Checking length of responses list to determine next route"""
+    if (len(responses) == len(survey.questions)):
+        return redirect('/done')
+    else:
+        return redirect(f'/questions/{len(responses)}')
+
+
+@app.route('/done')
+def show_thanks():
+    """Shows thank you message to user once survey is complete"""
+    return render_template('completed.html')
